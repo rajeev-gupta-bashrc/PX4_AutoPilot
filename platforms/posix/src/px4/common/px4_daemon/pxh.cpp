8c7c8aff5e6bc603ed7f14b09f0ebe2f3bf33363
rajeev-gupta-bashrc <rajeev@ph.iitr.ac.in>
2024-12-03 22:49:00 +0530

    failure control hover : LQR

--- platforms/posix/src/px4/common/px4_daemon/pxh.cpp
+++ platforms/posix/src/px4/common/px4_daemon/pxh.cpp
@@ -49,7 +49,6 @@
 #include <poll.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <stdlib.h>
 
 #include "pxh.h"
 
@@ -61,9 +60,6 @@ Pxh *Pxh::_instance = nullptr;
 
 Pxh::Pxh()
 {
-	_history.try_to_add("smf start 1"); // for convenience
-	_history.try_to_add("smf sub"); // for convenience
-	_history.try_to_add("smf detect"); // for convenience
 	_history.try_to_add("commander takeoff"); // for convenience
 	_history.reset_to_end();
 }
@@ -127,9 +123,6 @@ int Pxh::process_line(const std::string &line, bool silently_fail)
 	} else if (command == "help") {
 		list_builtins(_apps);
 		return 0;
-
-	} else if (command == "kill") {
-		return shutdown_handler(0, nullptr);
 
 	} else if (command.length() == 0 || command[0] == '#') {
 		// Do nothing
@@ -556,20 +549,6 @@ void Pxh::_tab_completion(std::string &mystr)
 			}
 		}
 	}
-}
-
-int Pxh::shutdown_handler(int argc, char *argv[])
-{
-	printf("Force shutting down...\n");
-	fflush(stdout);
-
-	// Force kill all child processes
-	int ret = system("pkill -9 -f gz");
-	(void)ret;  // Suppress warning about unused return value
-	printf("Killed gz...\n");
-	ret = system("pkill -9 -f px4");
-	(void)ret;  // Suppress warning about unused return value
-	return 0;
 }
 
 } // namespace px4_daemon
